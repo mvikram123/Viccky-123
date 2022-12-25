99e303b1568e4f50af6694174e6a4347c9cd9348
viccky8578 <108965142+viccky8578@users.noreply.github.com>
2022-12-26 01:17:12 +0530

    Add files via upload

--- worldDictionary/src/main/java/com/example/worlddictionary/DictionaryPage.java
+++ worldDictionary/src/main/java/com/example/worlddictionary/DictionaryPage.java
@@ -1,15 +1,19 @@
 package com.example.worlddictionary;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+
 
 import java.io.IOException;
 
  public class DictionaryPage {
+
      public AnchorPane root;
      DictionaryPage() throws IOException {
 
-        // String name;
          root = FXMLLoader.load(getClass().getResource("DictionaryPage.fxml"));
+
 
      }
  }
--- worldDictionary/src/main/java/com/example/worlddictionary/Main.java
+++ worldDictionary/src/main/java/com/example/worlddictionary/Main.java
@@ -1,26 +1,25 @@
 package com.example.worlddictionary;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+//import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+
 import javafx.scene.Group;
 
 public class Main extends Application {
     public static Group root;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+    public void start(Stage stage) throws Exception {
+
         root = new Group();
         stage.setTitle(" world Dictionary");
-        DictionaryPage page = new DictionaryPage();
+        DictionaryPage page;
+        page = new DictionaryPage();
         root.getChildren().add(page.root);
-        Scene scene = new Scene(root, 500, 500);
-        stage.setTitle("world dictionary");
-        stage.setScene(scene);
+
+        stage.setScene(new Scene(root,500,500));
         stage.show();
     }
 
